8c6dfddb5f744828464a0a799114dc03489d1e72
iMalFect <me@imal.dev>
2023-03-26 18:05:15 +0200

    whoops

--- KasNodeMon/out/server/routes/example.js
+++
@@ -1,5 +0,0 @@
-import {router} from '../index.js';
-
-export default router.get('/api/example', function(req, res) {
-  res.json('Hello from Kaspa Node Monitor!');
-});
--- KasNodeMon/out/server/routes/status.js
+++
@@ -1,7 +0,0 @@
-// Status
-import {router} from '../index.js';
-export default router.get('/api/status', (req, res) => {
-  res.json({
-    status: 'OK',
-  });
-});
